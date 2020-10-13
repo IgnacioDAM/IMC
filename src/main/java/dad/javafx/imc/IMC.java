9f40f1d353f53e0fb42c8b7901604f4917fe2a17
Ignacio <joseignacio.perezdelgado@iessantaana.com>
2020-10-13 10:05:19 +0100

    Update IMC.java

--- src/main/java/dad/javafx/imc/IMC.java
+++ src/main/java/dad/javafx/imc/IMC.java
@@ -1,5 +1,6 @@
 package dad.javafx.imc;
 
+
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
@@ -15,39 +16,73 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class IMC extends Application {
-
-	private Label pesoLabel, kilosLabel, alturaLabel, centimetrosLabel, imcLabel, estadoCorporalLabel;
-	private TextField pesoText, alturaText;
-
+	
 	private DoubleProperty pesoProperty = new SimpleDoubleProperty(0);
 	private DoubleProperty alturaProperty = new SimpleDoubleProperty(0);
 	private DoubleProperty imcProperty = new SimpleDoubleProperty(0);
-	private StringProperty estadoCorporalProperty = new SimpleStringProperty();
+	private StringProperty simcProperty = new SimpleStringProperty(this, "imc");
+	private StringProperty estadoCorporalProperty = new SimpleStringProperty(this, "estado");
 
-	public void cambiarEstadoCorporal() {
-		if (imcProperty.get() < 18.5)
-			estadoCorporalProperty.set("Bajo Peso");
-		else if (imcProperty.get() >= 18.5 && imcProperty.get() < 25)
-			estadoCorporalProperty.set("Normal");
-		else if (imcProperty.get() >= 25 && imcProperty.get() < 30)
-			estadoCorporalProperty.set("Sobrepeso");
-		else
-			estadoCorporalProperty.set("Obeso");
+	private void calcularIMC() {
+		if ((pesoProperty.get() == 0) || (alturaProperty.get() == 0))
+			imcProperty.set(0);
+		else {
+			double peso, altura, resultado;
+			peso = pesoProperty.get();
+			altura = alturaProperty.get();
+			resultado = (peso / (altura * altura));
+			imcProperty.set(Math.round(resultado * 100.00) / 100.00);
+		}
+	}
+
+	public void obtenerEstado() {
+		
+		if (imcProperty.get() == 0) {
+			simcProperty.set("IMC: (peso * altura ^ 2)");
+			estadoCorporalProperty.set("Bajo peso / Normal / Sobrepeso / Obeso");
+		} else {
+			simcProperty.set("IMC: " + imcProperty.get());
+			if (imcProperty.get() < 18.5)
+				estadoCorporalProperty.set("Bajo Peso");
+			else if (imcProperty.get() >= 18.5 && imcProperty.get() < 25)
+				estadoCorporalProperty.set("Normal");
+			else if (imcProperty.get() >= 25 && imcProperty.get() < 30)
+				estadoCorporalProperty.set("Sobrepeso");
+			else
+				estadoCorporalProperty.set("Obeso");
+		}
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		obtenerEstado();
+		
+		Label pesoLabel = new Label("Peso: ");
+		Label kilosLabel = new Label(" Kg");
+		Label alturaLabel = new Label("Altura: ");
+		Label centimetrosLabel = new Label(" m");
+		Label imcLabel = new Label("IMC: (peso * altura ^ 2");
+		Label estadoCorporalLabel = new Label("Bajo peso / Normal / Sobrepeso / Obeso");
 
-		pesoLabel = new Label("Peso: ");
-		kilosLabel = new Label(" Kg");
-		alturaLabel = new Label("Altura: ");
-		centimetrosLabel = new Label(" cm");
-
-		pesoText = new TextField();
+		TextField pesoText = new TextField();
 		pesoText.setPrefColumnCount(3);
-
-		alturaText = new TextField();
+		pesoText.setMaxWidth(100);
+		pesoText.setAlignment(Pos.CENTER);
+		
+		TextField alturaText = new TextField();
 		alturaText.setPrefColumnCount(3);
+		alturaText.setMaxWidth(100);
+		alturaText.setAlignment(Pos.CENTER);
+
+		pesoText.textProperty().bindBidirectional(pesoProperty, new NumberStringConverter());
+		pesoProperty.addListener((o, ov, nv) -> calcularIMC());
+
+		alturaText.textProperty().bindBidirectional(alturaProperty, new NumberStringConverter());
+		alturaProperty.addListener((o, ov, nv) -> calcularIMC());
+
+		imcLabel.textProperty().bindBidirectional(simcProperty);
+		estadoCorporalLabel.textProperty().bindBidirectional(estadoCorporalProperty);
+		imcProperty.addListener((o, ov, nv) -> obtenerEstado());
 
 		HBox pesobox = new HBox();
 		pesobox.setAlignment(Pos.BASELINE_CENTER);
@@ -57,33 +92,15 @@ public class IMC extends Application {
 		alturabox.setAlignment(Pos.BASELINE_CENTER);
 		alturabox.getChildren().addAll(alturaLabel, alturaText, centimetrosLabel);
 
-		pesoText.textProperty().bindBidirectional(pesoProperty, new NumberStringConverter());
-		alturaText.textProperty().bindBidirectional(alturaProperty, new NumberStringConverter());
-
-		// Antes de aqui esta bien
-
-		imcLabel = new Label("IMC : (peso * altura ^ 2");
-		estadoCorporalLabel = new Label("Bajo peso / Normal / Sobrepeso / Obeso");
-
-		imcProperty.bind((pesoProperty.divide((alturaProperty.multiply(alturaProperty)))));
-
-		imcLabel.textProperty().bind(imcProperty.asString());
-
 		HBox imcbox = new HBox();
 		imcbox.setAlignment(Pos.BASELINE_CENTER);
 		imcbox.getChildren().addAll(imcLabel);
 		imcbox.setSpacing(5);
 
-		estadoCorporalLabel.textProperty().bind(estadoCorporalProperty);
-		pesoProperty.addListener((o, ov, nv) -> cambiarEstadoCorporal());
-		alturaProperty.addListener((o, ov, nv) -> cambiarEstadoCorporal());
-
 		HBox estadoCorporalbox = new HBox();
 		estadoCorporalbox.setAlignment(Pos.BASELINE_CENTER);
 		estadoCorporalbox.getChildren().addAll(estadoCorporalLabel);
 		estadoCorporalbox.setSpacing(5);
-
-		// A partir de aqui esta bien
 
 		VBox root = new VBox();
 		root.setAlignment(Pos.CENTER);
